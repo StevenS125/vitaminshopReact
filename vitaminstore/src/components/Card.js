1c92754a4960aa4e3d32037d9fcfa60dbf7ab35a
StevenS125 <sts12589@gmail.com>
2018-03-15 16:06:50 -0400

    created basic background, containers, click elements

---
+++ vitaminstore/src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => (
+    <div className= {props.card.animation}
+    onClick={() => props.clickCard(props.card)} 
+    >
+    <img src='juice.jpg' alt="vitamin juice" className="card-image" />
+    <div className="container">
+    <h3>Vitamin Juice <span className="price">$24.99</span></h3>
+    <p>Need a jump on your vitamins while drinking? Tired of popping pills?
+        Drink our vitamin enhanced juice, available in several flavours. </p>
+    </div>
+    </div>
+);
+
+export default Card
