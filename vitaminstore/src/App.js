940ed1551f41a607487a8bf1c0dd1700ab13084b
StevenS125 <sts12589@gmail.com>
2018-03-15 22:39:41 -0400

    partial build, lazy load introduced to card elements

--- vitaminstore/src/App.js
+++ vitaminstore/src/App.js
@@ -1,72 +1,103 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
-import Card from "./components/Card";
+import Card from './components/Card';
+import Loading from './components/Loading';
+import Navigation from './components/Navigation';
+import data from './data/data.json';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       toggleLogo: true,
-      cards: [ 
-        { id: 0,
-          animation: 'card'
-        },
-        { id: 1,
-          animation: 'card'
-        },
-        { id: 2,
-          animation: 'card'
-        },
-        { id: 3,
-          animation: 'card'
-        },
-        { id: 4,
-          animation: 'card'
-        },
-        { id: 5,
-          animation: 'card'
-        }],
+      loading: true,
+      cards: [],
     }
     this.toggleLogo = this.toggleLogo.bind(this);
-    this.clickCard = this.clickCard.bind(this);
+    this.openNav = this.openNav.bind(this);
+    this.closeNav = this.closeNav.bind(this);
+    this.showBack = this.showBack.bind(this);
+    this.showFront = this.showFront.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({
+      cards: data,
+    });
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ loading: false}), 3000);
   }
 
   toggleLogo(event) {
-    this.setState((prevState)=> ({
+    this.setState((prevState) => ({
       toggleLogo: !prevState.toggleLogo
-    })); 
+    }));
   }
 
-clickCard(card) {
-  let cards = this.state.cards;
-  cards[card.id].animation = "card animated zoomOut";
-  console.log(cards);
+  showBack(card) {
+    let cards = this.state.cards;
+    cards[card.id].animation = 'card card-flip';
+    console.log(cards);
 
-  this.setState({
-    cards,
-  })
-}
+    this.setState({
+      cards,
+    });
+  }
+
+  showFront(card) {
+    let cards = this.state.cards;
+    cards[card.id].animation = 'card';
+    console.log(cards);
+
+    this.setState({
+      cards,
+    });
+  }
+
+  openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+
+  closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} 
-          className={this.state.toggleLogo ? 'static-logo' : 'static-logo animated jello'} 
-          alt="logo"
-          onMouseEnter={this.toggleLogo} 
-          onMouseLeave={this.toggleLogo}
+            className={this.state.toggleLogo ? 'static-logo' : 'static-logo animated jello'} 
+            alt="logo"
+            onMouseEnter={this.toggleLogo}
+            onMouseLeave={this.toggleLogo}
+            onClick={this.openNav}
           />
-          <h1 className="App-title">Welcome to React</h1>
+        <h1 
+        className={this.state.toggleLogo ? "menu-hidden" : "menu animated bounceInDown"}
+        onClick={this.openNav}
+        >Menu</h1>
+          <Navigation closeNav={this.closeNav} />
         </header>
-        <div className="Grid">
         {
-          this.state.cards.map((card) => (
-              <Card duration={150} key={card.id} card={card} clickCard={this.clickCard}/>
-          ))
+          this.state.loading ? <Loading /> :
+          <div className="Grid animated bounceInUp">
+          {
+            this.state.cards.map((card) => (
+              <Card 
+                duration={150} 
+                key={card.id} 
+                card={card} 
+                showBack={this.showBack}
+                showFront={this.showFront}
+              />
+            ))
+          }
+        </div>
         }
-          </div>
       </div>
     );
   }
